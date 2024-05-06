9b56cabac02fd3bc2b2a8b287a552cef4abae6f8
Michał Burda <163707785+MichalBurda4@users.noreply.github.com>
2024-05-06 13:09:52 +0200

    Update CodeExample.java

--- CodeExample.java
+++ CodeExample.java
@@ -1,1 +1,33 @@
+private class test{
+    private string myString = "abc";
+    public int myInt = 4 * 5;
 
+    private void run(){
+        int x = 0;
+        x = myInt;
+        string myString2;
+        string my = myString2;
+        x = 5 * myInt;
+    }
+}
+
+
+class test2{
+    private void print(){
+        string wypiszNaEkran = "abc xyz"; 
+    }
+}
+
+class test3{
+    protected void print(){
+        string wypiszNaEkran = "siema jestem marek"; 
+    }
+}
+
+class tablica{
+    protected void print(){
+        int[] numbers = new int[5]; 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        } 
+    }
+}
