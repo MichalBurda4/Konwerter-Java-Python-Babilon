33e6d106af1738175e83cc590f5331a0dd0cc80c
Radosław Barszczak <168109148+kKedaRr3@users.noreply.github.com>
2024-06-05 19:32:50 +0200

    Add files via upload

---
+++ Konwerter_v6/views/temp.py
@@ -0,0 +1,14 @@
+
+class BubbleSort:
+
+    def bubbleSort(self, array):
+        n = None
+        n = array.length
+        temp = None
+        for i in range(n-1):
+            for j in range(n-i-1):
+                if array[j]>array[j+1]:
+                    temp = array[j]
+                    array[j] = array[j+1]
+                    array[j+1] = temp
+
